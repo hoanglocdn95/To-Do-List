86933dd8924fa9300468e549b6759beeb080d27a
hoanglocdn95 <hoanglocdn95@gmail.com>
2018-09-20 09:50:08 +0700

    20/9 9:49am edit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     this.delete = this.delete.bind(this);
     this.edit = this.edit.bind(this);
     this.add = this.add.bind(this);
+    this.save = this.save.bind(this);
     this.state = {
       list: [
         {key:1,value:'list-1'},
@@ -18,10 +19,33 @@ class App extends Component {
       ]
     };
   };
-  edit =()=>{
-    
+  edit =(id)=>{
+    let arrDiv = document.getElementsByClassName('input-list');
+    let arrInput = document.getElementsByClassName('input-edit');
+    let arrSave = document.getElementsByClassName('button-save');
+    arrInput[id].style.display = 'inline-block'; 
+    arrDiv[id].style.display ='none';
+    arrSave[id].style.display = 'inline-block';
   };
-  
+  save =(id)=>{
+    const arr = this.state.list;
+    const arr2 = {
+                    key: id+1, 
+                    value: document.getElementsByClassName('input-edit')[id].value
+                  }; 
+    arr[id] = arr2;
+    this.setState({
+      list: arr
+    });
+      console.log('sau '+this.state.list[id].key);
+      
+      let arrDiv = document.getElementsByClassName('input-list');
+      let arrInput = document.getElementsByClassName('input-edit');
+      let arrSave = document.getElementsByClassName('button-save');
+      arrInput[id].style.display = 'none'; 
+      arrDiv[id].style.display ='inline-block';
+      arrSave[id].style.display = 'none';
+  }
   add = () =>{
     var a = document.getElementById('value').value;
     const arr = this.state.list;
@@ -40,11 +64,14 @@ class App extends Component {
     });
   };
   render() {
-    let showList = this.state.list.map((list)=>{
+    let showList = this.state.list.map((list, index)=>{
       return(
         <List dataList={list.value} 
               onClickEdit={this.edit} 
-              onClickDel={this.delete}/>
+              onClickDel={this.delete}
+              onClickSave={this.save}
+              indexList={index}
+        />
       );
     });
     return (
